aaa6de7c7e7fc476ce5f6cfefff75a31b95c2902
taeho <taeho9421@naver.com>
2021-02-15 19:23:51 +0900

    vuejs-todolist02

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fas fa-plus addBtn"/>
+    </span>
+
+  <modal v-if="showModal" @close="showModal = false">
+
+    <h3 slot="header">
+      경고!
+    <i class="closeModalBtn far fa-times" @click="showModal=false"></i>  
+    </h3>
+    <h3 slot="body">무언가를 입력해주세요.</h3>
+    
+  </modal>
+  </div>
+</template>
+
+<script>
+import Modal from './common/Modal.vue'
+
+export default {
+  data: function() {
+    return {
+      newTodoItem : "",
+      showModal:false
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if(this.newTodoItem!==''){
+        this.$emit('addTodoItem',this.newTodoItem);
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput: function() {
+      this.newTodoItem='';
+    }
+  },
+  components: {
+    'Modal':Modal
+  }
+}
+</script>
+
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background-color: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
+  .closeModalBtn {
+    color:#42b983;
+    
+  }
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="(todoItem,index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+          <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted:todoItem.completed}" v-on:click="toggleComplete(todoItem,index)"></i>
+        <span v-bind:class="{textCompleted:todoItem.completed}">{{ todoItem.item }}</span> 
+        <span class="removeBtn" v-on:click="removeTodo(todoItem,index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['propsdata'],
+  methods: {
+    removeTodo:function(todoItem,index) {
+      this.$emit('removeItem',todoItem,index);
+    },
+    toggleComplete:function(todoItem,index) {
+      todoItem.comleted = !todoItem.completed;
+      this.$emit('toggleItem',todoItem,index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0px;
+  text-align: left;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin:0.5rem 0;
+  padding: 0 0.5rem;
+  background:white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left:auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;;
+}
+</style>
