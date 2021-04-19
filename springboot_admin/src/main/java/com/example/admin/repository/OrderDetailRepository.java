f5e0534310e94d81f8336c304830306a57e4a0b6
taeho9421 <69471128+taeho9421@users.noreply.github.com>
2021-04-20 00:39:17 +0900

    Delete springboot_admin directory

--- springboot_admin/src/main/java/com/example/admin/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin.repository;
-
-import com.example.admin.model.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail,Long> {
-}
