4a57a12c3a27720f3de046f80e0af2399079129d
taeho <taeho9421@naver.com>
2021-04-27 23:40:00 +0900

    springboot_JPA_exercise

---
+++ bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
@@ -0,0 +1,44 @@
+package com.fastcampus.jpa.bookmanager.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+//@Getter
+//@Setter
+//@ToString
+@NoArgsConstructor // 인자가 없는 생성자
+@AllArgsConstructor // 필드에있는 모든 인자를 받아서 생성하는 생성자
+@RequiredArgsConstructor // 필요한 인자만을 이용하여 생성자를 만들어줌
+//@EqualsAndHashCode
+@Data // Entity 객체를 사용하기위해 제일 많이 사용되는 어노테이션
+@Builder
+@Entity
+public class User {
+    @Id // primary key값
+    @GeneratedValue
+    private Long id;
+
+    @NonNull // 필수값을 지정할떄
+    private String name;
+
+    @NonNull
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+//    @Override
+//    public String toString() {
+//        return getClass().getName()
+//                +" : "
+//                +"name= " +name
+//                +", email= "+email
+//                +", createdAt= "+createdAt
+//                +", updatedAt= "+updatedAt;
+//    }
+}
---
+++ bookmanager/src/test/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into user('id','name','email','created_at','updated_at') values (1,'taeho','taeho@naver.com',now(),now());
+
+call next value for hibernate_sequence;
+insert into user('id','name','email','created_at','updated_at') values (2,'dennis','dennis@nate.com',now(),now());
+
+call next value for hibernate_sequence;
+insert into user('id','name','email','created_at','updated_at') values (3,'sophia','sopi@gamail.com',now(),now());
+
+call next value for hibernate_sequence;
+insert into user('id','name','email','created_at','updated_at') values (4,'james','js@cju.co.kr',now(),now());
+
+call next value for hibernate_sequence;
+insert into user('id','name','email','created_at','updated_at') values (5,'taeho','another@nexon.com',now(),now());
